d01e4e5892c36b090c06fb383721e70810d91edc
Shakir Ahmed Zahedi <shakirahmed84@yahoo.com>
2024-11-23 22:15:03 +0100

    Initial commit to create a Spring boot project.

---
+++ src/main/java/com/example/comeonplayerserviceassignment/ComeOnPlayerServiceAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.comeonplayerserviceassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComeOnPlayerServiceAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComeOnPlayerServiceAssignmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/comeonplayerserviceassignment/ComeOnPlayerServiceAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.comeonplayerserviceassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComeOnPlayerServiceAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
